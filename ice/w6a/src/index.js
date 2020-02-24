3fcaa2509e390f526c5c6dc160f98ddf25b085dc
Sarah Sinvil <sarah.alise98@gmail.com>
2020-02-23 21:56:50 -0500

    ice 6

--- ice/w6a/src/index.js
+++ ice/w6a/src/index.js
@@ -13,10 +13,10 @@ function Square(props) {
   
 class Board extends React.Component {
     constructor(props){
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
+      super(props);
+      this.state = {
+         squares: Array(9).fill(null),
+          xIsNext: true,
         };
     }
     
