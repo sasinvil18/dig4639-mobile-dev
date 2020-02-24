37073913ad603ab4227b8c18665171795131647e
Sarah Sinvil <sarah.alise98@gmail.com>
2020-02-23 23:58:48 -0500

    lab 3

--- labs/3/src/components/Card/index.js
+++ labs/3/src/components/Card/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 
 class Card extends React.Component{
     constructor(props){
         super(props);
         this.element = document.createElement('div');
-        this.element.innerHTML=this.element.content;
-
-
-    }
-    render(){
-        return;
+        this.element.className = "card";
+        this.cont.innerHTML= this.element.content;
+        document.body.element.appendChild(this.cont);
+        document.body.appendChild(this.element);
     }
 }
 
 export default Card;
+
+ReactDOM.render(<Card />, document.getElementById('root'));
