f24b96c24ca695db47a8d4b0be039a06d99dbbda
Sarah Sinvil <sarah.alise98@gmail.com>
2020-03-01 23:53:06 -0500

    lab 4

--- labs/4/src/components/Card/index.js
+++ labs/4/src/components/Card/index.js
@@ -5,11 +5,13 @@ import './index.css';
 class Card extends React.Component{
     constructor(props){
         super(props);
-        this.element = document.createElement('div');
+        /*this.element = document.createElement('div');
         this.element.className = "card";
         this.cont.innerHTML= this.element.content;
         document.body.element.appendChild(this.cont);
-        document.body.appendChild(this.element);
+        document.body.appendChild(this.element);*/
+        this.element = document.createElement('h3');
+        this.content = document.createTextNode("This is a title")
     }
 }
 
--- labs/4/src/components/CardList.js
+++ labs/4/src/components/CardList.js
@@ -6,9 +6,14 @@ import './index.css';
 class Close extends Card{
     constructor(props){
         super(props);
+        //import cards with for loop
+        //remove button for each element
         this.element = document.createElement('button');
         this.element.className = "close";
-        this.element.onclick =
+        //this.element.onclick =
+        //<button className="button" 
+        //onClick={(remove card) => {this.changeMenu(event, 1)} }>
+        //</button>
         document.body.appendChild(this.element);
     }
 }
