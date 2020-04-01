418192051c0cde897dedc45019136c897fb24e3f
Sarah Sinvil <sarah.alise98@gmail.com>
2020-03-31 22:23:27 -0400

    lab 5 commit

--- labs/5/src/App.js
+++ labs/5/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/index.js';
+import './components/Card/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Card {
+  /*render(){
+    return (
+      <Card content="This is a card!"></Card>
+    );
+  }
+}*/
+
+state = {
+
+};
+
+async componentDidMount() {
+  const data = "https://api.weather.gov/gridpoints/MLB/25,69/forecast";
+  const obtained = await fetch(data);
+  const dataObtained = await obtained.json();
+}
+
+render(){
+  return(
+    <div>
+      
     </div>
   );
 }
+
+
 
 export default App;
