683719afa1098461d664227b6d3de863cf11dfa4
Sarah Sinvil <sarah.alise98@gmail.com>
2020-04-05 23:59:35 -0400

    lab 6 Native

--- labs/6/App.js
+++ labs/6/App.js
@@ -1,49 +1,14 @@
-import React from 'react';
-import { Platform, StyleSheet, Text, View, Button} from 'react-native';
-//import "./comp./components/Questions";
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import homeScreen from "./components/homeScreen";
+import Play from "./components/playGame";
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
+const MainNavigator = createStackNavigator({
+  Home: {screen: homeScreen},
+  Play: {screen: Play}
 });
 
-export default class App extends React.Component {
-  render(){
+const App = createAppContainer(MainNavigator);
 
-    const {navigate} = this.props.navigation;
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to The Mobile Development Review Game!</Text>
-        <Button
-          style = {styles.button}
-          title = "Play"
-          onClick = {() => navigate("playGame")}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#add8e6',
-  },
-  welcome: {
-    fontSize: 40,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  button: {
-    color: '#800000',
-  },
-});
+export default App;
 
---
+++ labs/6/components/homeScreen/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Platform, StyleSheet, Text, View, Button} from 'react-native';
+//import "./comp./components/Questions";
+
+const instructions = Platform.select({
+  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
+  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
+});
+
+export default class Home extends React.Component {
+  render(){
+
+    const {navigate} = this.props.navigation;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to The Mobile Development Review Game!</Text>
+        <Button
+          style = {styles.button}
+          title = "Play"
+          onClick = {() => navigate("playGame")}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#add8e6',
+  },
+  welcome: {
+    fontSize: 40,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  button: {
+    color: '#800000',
+  },
+});
